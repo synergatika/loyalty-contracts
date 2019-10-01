c359eb1cecdf7c3ad37f55a9b500c070d3bd31a3
George Theofilis <theofilis.g@gmail.com>
2019-10-01 23:24:14 +0300

    Test member & partner registration

--- test/loyalty.spec.js
+++ test/loyalty.spec.js
@@ -1,6 +1,6 @@
 const LoyaltyPoints = artifacts.require("LoyaltyPoints");
 const PointsTokenStorageProxy = artifacts.require("PointsTokenStorageProxy");
-
+const assertRevert = require('./helpers/assertRevert');
 
 contract("Proxy", (accounts) => {
     let options = {};
@@ -19,9 +19,66 @@ contract("Proxy", (accounts) => {
             })
 
             it('returns true', async function () {
-                const initialized = await loyalty.owner();
-                assert.equal(initialized, accounts[0]);
+                const owner = await loyalty.owner();
+                assert.equal(owner, accounts[0]);
+            })
+
+            it('returns 0', async function () {
+                const transactionsInfoLength = await loyalty.transactionsInfoLength();
+                assert.equal(transactionsInfoLength, 0);
+            })
+
+            it('returns 0', async function () {
+                const partnersInfoLength = await loyalty.partnersInfoLength();
+                assert.equal(partnersInfoLength, 0);
             })
         })
+
+        describe('Register a member', function () {
+            // it('success', async function () {
+            //     const result = await loyalty.registerMember();
+            //     assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            // })
+
+            // it('revert', async function () {
+            //     await assertRevert(loyalty.registerMember());
+            // })
+
+            it('success', async function () {
+                const result = await loyalty.registerMember({ from: accounts[1] });
+                assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            })
+
+            it('revert', async function () {
+                await assertRevert(loyalty.registerMember({ from: accounts[1] }));
+            })
+
+            it('success', async function () {
+                const result = await loyalty.registerMember(accounts[2]);
+                assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            })
+        })
+
+        describe('Register a Partner', function () {
+            it('success', async function () {
+                const result = await loyalty.registerPartner();
+                assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            })
+
+            it('success', async function () {
+                const result = await loyalty.registerPartner(accounts[6]);
+                assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            })
+
+            it('success', async function () {
+                const result = await loyalty.registerPartner(accounts[5]);
+                assert.equal('0x01', result.receipt.status, 'valid proof creation');
+            })
+
+            it('revert', async function () {
+                await assertRevert(loyalty.registerPartner(accounts[5]));
+            })
+        })
+
     });
 });
