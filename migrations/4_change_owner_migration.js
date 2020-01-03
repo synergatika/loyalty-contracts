68562b245a228811aaa6b6f4ab3ce3d92df5141f
George Theofilis <theofilis.g@gmail.com>
2020-01-03 02:04:08 +0200

    Fix error on migration

--- migrations/4_change_owner_migration.js
+++ migrations/4_change_owner_migration.js
@@ -5,8 +5,14 @@ const PointsTokenStorageProxy = artifacts.require("PointsTokenStorageProxy");
 module.exports = function (deployer, network, accounts) {
   let loyalty, proxy;
   deployer
-    .then((instance) => {
+    .then(() => {
         return PointsTokenStorageProxy.deployed();
+    })
+    .then((instance) => {
+        return instance.address;
+    })
+    .then((address) => {
+        return LoyaltyPoints.at(address);
     })
     .then((proxy) => {
         return proxy.transferOwnership('0x627306090abaB3A6e1400e9345bC60c78a8BEf57', { from: accounts[0] });
